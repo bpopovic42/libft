74c9f5ba0dcd7e68d55e6cc5de43eee4853d55f9
bpopov <byurinov1@gmail.com>
2018-09-19 03:11:52 +0200

    [FIX] Weird conditional mistake

--- srcs/ft_str/ft_strncat.c
+++ srcs/ft_str/ft_strncat.c
@@ -6,7 +6,7 @@
 /*   By: bopopovi <bopopovi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/04/09 12:37:34 by bopopovi          #+#    #+#             */
-/*   Updated: 2018/05/30 17:47:25 by bopopovi         ###   ########.fr       */
+/*   Updated: 2018/09/19 03:02:16 by bopopovi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,8 +18,6 @@ char	*ft_strncat(char *s1, const char *s2, size_t n)
 
 	ptr = s1;
 	if (s1 == NULL || s2 == NULL)
-		return (NULL);
-	if (n > ft_strlen(s2))
 		return (NULL);
 	while (*s1)
 		s1++;
