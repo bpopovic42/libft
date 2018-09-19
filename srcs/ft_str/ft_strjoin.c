eb8efef45572853e6130cbe79fc23e05c8d4c94f
bpopov <byurinov1@gmail.com>
2018-09-19 03:11:36 +0200

    [FIX] Strange use of the ft_strncat function

--- srcs/ft_str/ft_strjoin.c
+++ srcs/ft_str/ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: bopopovi <bopopovi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/04/19 15:35:28 by bopopovi          #+#    #+#             */
-/*   Updated: 2018/04/25 16:30:50 by bopopovi         ###   ########.fr       */
+/*   Updated: 2018/09/19 03:01:27 by bopopovi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,6 +23,6 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	if (!(dst = ft_strnew(s1_len + s2_len)))
 		return (NULL);
 	dst = ft_strncpy(dst, s1, s1_len);
-	dst = ft_strncat(dst, s2, (s1_len + s2_len));
+	dst = ft_strncat(dst, s2, s2_len);
 	return (dst);
 }
