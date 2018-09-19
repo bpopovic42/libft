bc8a9af67b51a5a7931de6cfb0b86955fcb98076
bpopov <byurinov1@gmail.com>
2018-09-19 03:44:13 +0200

    [FIX] Strncat or strcnpy returning NULL would override dest initial allocated address

--- srcs/ft_str/ft_strjoin.c
+++ srcs/ft_str/ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: bopopovi <bopopovi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/04/19 15:35:28 by bopopovi          #+#    #+#             */
-/*   Updated: 2018/09/19 03:01:27 by bopopovi         ###   ########.fr       */
+/*   Updated: 2018/09/19 03:39:36 by bopopovi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,11 +18,11 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	size_t	s1_len;
 	size_t	s2_len;
 
-	s1_len = ft_strlen(s1);
-	s2_len = ft_strlen(s2);
+	s1_len = s1 ? ft_strlen(s1) : 0;
+	s2_len = s2 ? ft_strlen(s2) : 0;
 	if (!(dst = ft_strnew(s1_len + s2_len)))
 		return (NULL);
-	dst = ft_strncpy(dst, s1, s1_len);
-	dst = ft_strncat(dst, s2, s2_len);
+	ft_strncpy(dst, s1, s1_len);
+	ft_strncat(dst, s2, s2_len);
 	return (dst);
 }
