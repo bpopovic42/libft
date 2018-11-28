f0bf3795284effbf1a1c62aafdd7b403e7a204f8
Boris POPOVIC <bopopovi@student.42.fr>
2018-11-28 20:48:34 +0100

    [FIX][MOD] Modified strsplit to handle a string of splitting characters instead of a single split character which consequently fixed the issue with sending a single character's address to the count_words function

--- srcs/ft_str/ft_strsplit.c
+++ srcs/ft_str/ft_strsplit.c
@@ -6,20 +6,20 @@
 /*   By: bopopovi <bopopovi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/04/20 14:16:50 by bopopovi          #+#    #+#             */
-/*   Updated: 2018/11/27 19:26:28 by bopopovi         ###   ########.fr       */
+/*   Updated: 2018/11/28 16:47:43 by bopopovi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include <stdlib.h>
 
-char			**ft_strsplit(char const *s, char c)
+char			**ft_strsplit(char const *s, char *split)
 {
 	size_t	i;
 	size_t	size;
 	char	**res;
 
-	size = ft_count_words(s, &c);
+	size = ft_count_words(s, split);
 	if (!(res = (char**)malloc(sizeof(*res) * (size + 1))))
 		return (NULL);
 	res[size] = NULL;
@@ -27,11 +27,11 @@ char			**ft_strsplit(char const *s, char c)
 	while (*s)
 	{
 		i = 0;
-		if (*s == c)
+		if (ft_strchr(split, *s))
 			s++;
-		else if (s[i] != c)
+		else
 		{
-			while (s[i] && s[i] != c)
+			while (s[i] && !ft_strchr(split, s[i]))
 				i++;
 			if (!(res[size] = ft_strsub(s, 0, i)))
 				return (NULL);
