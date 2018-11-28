6891dd3e6534e8f4755d4b3310f29a8743c50a30
Boris POPOVIC <bopopovi@student.42.fr>
2018-11-28 20:50:31 +0100

    [FIX] Function should proceed normally as long as the sub-string len is smaller or EQUAL to the lenght of the source string, also fixed some possible type conflicts

--- srcs/ft_str/ft_strsub.c
+++ srcs/ft_str/ft_strsub.c
@@ -6,18 +6,19 @@
 /*   By: bopopovi <bopopovi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/04/19 15:15:33 by bopopovi          #+#    #+#             */
-/*   Updated: 2018/05/25 20:43:44 by bopopovi         ###   ########.fr       */
+/*   Updated: 2018/11/28 20:11:35 by bopopovi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+#include "ft_printf.h"
 
 char	*ft_strsub(char const *s, unsigned int start, size_t len)
 {
 	char	*dst;
 	size_t	i;
 
-	if (len < (ft_strlen(s) - start))
+	if (len <= (size_t)(ft_strlen(s) - (int)start))
 	{
 		if (!(dst = ft_strnew(len)))
 			return (NULL);
