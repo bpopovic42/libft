a4d9dbb4fc04a2c8e5d0e03ab7a0ad656771e6a2
Boris POPOVIC <bopopovi@student.42.fr>
2018-12-12 19:46:03 +0100

    [FIX] NULL initialized all hashtable's buckets

--- srcs/ft_hash/ft_hash_newtable.c
+++ srcs/ft_hash/ft_hash_newtable.c
@@ -6,7 +6,7 @@
 /*   By: bopopovi <bopopovi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/03 19:56:32 by bopopovi          #+#    #+#             */
-/*   Updated: 2018/12/03 20:24:22 by bopopovi         ###   ########.fr       */
+/*   Updated: 2018/12/12 19:33:14 by bopopovi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,16 @@
 
 t_htable	*ft_hash_newtable(size_t table_size)
 {
-	t_htable *new;
+	t_htable	*new;
+	size_t		i;
 
-	if (!(new = malloc(sizeof(t_htable*))))
+	i = 0;
+	if (!(new = malloc(sizeof(*new))))
 		ft_perror("ft_hash_newtable : Couldn't allocate structure.");
 	new->size = table_size;
-	if (!(new->table = malloc(sizeof(t_hash*) * table_size)))
+	if (!(new->table = malloc(sizeof(t_hash) * table_size)))
 		ft_perror("ft_hash_newtable : Couldn't allocate table.");
+	while (i < table_size)
+		new->table[i++] = NULL;
 	return (new);
 }
