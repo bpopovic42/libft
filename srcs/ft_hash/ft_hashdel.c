eee1948f5cde81d29bbf3bd978851875c20c3c9b
Boris POPOVIC <bopopovi@student.42.fr>
2018-12-13 20:21:08 +0100

    [MOD] Hash and its contents are now directly deleted, with the help of a generic function pointer for hash's data

--- srcs/ft_hash/ft_hashdel.c
+++ srcs/ft_hash/ft_hashdel.c
@@ -6,20 +6,18 @@
 /*   By: bopopovi <bopopovi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/05/25 00:16:59 by bopopovi          #+#    #+#             */
-/*   Updated: 2018/12/03 19:54:34 by bopopovi         ###   ########.fr       */
+/*   Updated: 2018/12/13 17:46:57 by bopopovi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	ft_hashdel(t_htable *table, char *key)
+void		ft_hashdel(t_hash *hash, void (*del)(void *))
 {
-	t_hash	*elem;
-
-	elem = ft_hashpop(table, key);
-	ft_strdel(&elem->key);
-	elem->data_size = 0;
-	elem->next = NULL;
-	ft_memdel(elem->data);
-	ft_memdel((void*)elem);
+	free(hash->key);
+	hash->key = NULL;
+	del(hash->data);
+	hash->data = NULL;
+	hash->data_size = 0;
+	free(hash);
 }
