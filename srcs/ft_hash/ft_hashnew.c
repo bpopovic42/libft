192c83e7463b613927618de24ab589e755647c48
Boris POPOVIC <bopopovi@student.42.fr>
2018-12-13 20:14:07 +0100

    [FIX] Choosing wether the data is duplicated or saved directly in the hash should be left to the user, therefore data is now taken as it is

--- srcs/ft_hash/ft_hashnew.c
+++ srcs/ft_hash/ft_hashnew.c
@@ -6,31 +6,22 @@
 /*   By: bopopovi <bopopovi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/05/24 23:44:37 by bopopovi          #+#    #+#             */
-/*   Updated: 2018/12/03 19:51:35 by bopopovi         ###   ########.fr       */
+/*   Updated: 2018/12/13 20:03:11 by bopopovi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include <stdlib.h>
 
-t_hash		*ft_hashnew(char *key, void const *data, size_t data_size)
+t_hash		*ft_hashnew(char *key, void *data, size_t data_size)
 {
 	t_hash *new;
 
 	if (!(new = (t_hash*)malloc(sizeof(*new))))
 		return (NULL);
 	new->next = NULL;
-	new->key = key;
-	if (data == NULL)
-	{
-		new->data = NULL;
-		new->data_size = 0;
-	}
-	else
-	{
-		if (!(new->data = ft_memdup(data, data_size)))
-			return (NULL);
-		new->data_size = data_size;
-	}
+	new->key = ft_strdup(key);
+	new->data = data;
+	new->data_size = data_size;
 	return (new);
 }
