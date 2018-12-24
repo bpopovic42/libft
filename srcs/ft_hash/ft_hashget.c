b4f287b415357fbbe407c10f04d906868622118d
bpopov <byurinov1@gmail.com>
2018-12-24 01:49:14 +0100

    [FIX] Potential issue if NULL key was provided

--- srcs/ft_hash/ft_hashget.c
+++ srcs/ft_hash/ft_hashget.c
@@ -6,7 +6,7 @@
 /*   By: bopopovi <bopopovi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/05/24 23:52:12 by bopopovi          #+#    #+#             */
-/*   Updated: 2018/12/03 19:40:48 by bopopovi         ###   ########.fr       */
+/*   Updated: 2018/12/24 00:21:46 by bopopovi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ t_hash	*ft_hashget(t_htable *table, char *key)
 {
 	int		hash;
 
-	if (table)
+	if (table && key)
 	{
 		hash = ft_hash(table, key);
 		return (table->table[hash]);
