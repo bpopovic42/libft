1917907ab9aed92a090d8a16cf5b234c5a2c0dc2
bpopov <byurinov1@gmail.com>
2018-12-25 15:46:50 +0100

    [FIX] Mindless error

--- srcs/ft_str/ft_is_valid_int.c
+++ srcs/ft_str/ft_is_valid_int.c
@@ -6,7 +6,7 @@
 /*   By: bopopovi <bopopovi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/24 00:42:54 by bopopovi          #+#    #+#             */
-/*   Updated: 2018/12/25 15:41:58 by bopopovi         ###   ########.fr       */
+/*   Updated: 2018/12/25 15:45:54 by bopopovi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ int		ft_is_valid_int(const char *nbr)
 	i = 0;
 	while (nbr[i])
 	{
-		if (i > 0 && ft_issign)
+		if (i > 0 && ft_issign(nbr[i]))
 			return (0);
 		else if (!ft_isdigit(nbr[i]) && !ft_issign(nbr[i]))
 			return (0);
