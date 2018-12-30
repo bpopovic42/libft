9f8907be2db7414777f75f14399caa5301a1327b
Boris POPOVIC <bopopovi@student.42.fr>
2018-12-30 20:26:25 +0100

    [MOD] Tried to improve vector initialization and access, changes need some further investigations, might have broken some stuff

--- srcs/ft_vector/ft_vector_get.c
+++ srcs/ft_vector/ft_vector_get.c
@@ -6,7 +6,7 @@
 /*   By: bopopovi <bopopovi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/20 04:28:23 by bopopovi          #+#    #+#             */
-/*   Updated: 2018/11/20 04:31:30 by bopopovi         ###   ########.fr       */
+/*   Updated: 2018/12/30 19:30:03 by bopopovi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 void	*ft_vector_get(t_vect *v, size_t index)
 {
-	if (index >= v->size)
-		ft_perror("ft_vector_get: index out of bounds");
+	if (!v || index >= v->capacity || index >= v->size)
+		return (NULL);
 	return (v->data[index]);
 }
--- srcs/ft_vector/ft_vector_init.c
+++ srcs/ft_vector/ft_vector_init.c
@@ -6,7 +6,7 @@
 /*   By: bopopovi <bopopovi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/20 02:50:01 by bopopovi          #+#    #+#             */
-/*   Updated: 2018/11/20 04:34:08 by bopopovi         ###   ########.fr       */
+/*   Updated: 2018/12/30 19:22:00 by bopopovi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,10 +20,11 @@ t_vect	*ft_vector_init(size_t data_size, size_t capacity)
 		ft_perror("ft_vector_init: vector allocation failed.");
 	if (capacity == 0)
 		capacity = VECTOR_INITIAL_SIZE;
+	else
+		new->capacity = capacity;
 	if (!(new->data = malloc(data_size * capacity)))
 		ft_perror("ft_vector_init: vector data's allocation failed.");
 	new->data_size = data_size;
 	new->size = 0;
-	new->capacity = capacity;
 	return (new);
 }
