0d9dd2d8598589278cf78be55e3bdd1f1ec078c0
bpopov <byurinov1@gmail.com>
2019-01-03 21:43:34 +0100

    [MOD][FIX?] Table contents are now null initialized at creation

--- srcs/ft_hash/ft_hash_newtable.c
+++ srcs/ft_hash/ft_hash_newtable.c
@@ -6,7 +6,7 @@
 /*   By: bopopovi <bopopovi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/03 19:56:32 by bopopovi          #+#    #+#             */
-/*   Updated: 2018/12/12 19:33:14 by bopopovi         ###   ########.fr       */
+/*   Updated: 2019/01/03 18:05:05 by bopopovi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,6 +23,7 @@ t_htable	*ft_hash_newtable(size_t table_size)
 	new->size = table_size;
 	if (!(new->table = malloc(sizeof(t_hash) * table_size)))
 		ft_perror("ft_hash_newtable : Couldn't allocate table.");
+	ft_bzero(new->table, sizeof(t_hash) * table_size);
 	while (i < table_size)
 		new->table[i++] = NULL;
 	return (new);
