e596641255f10fe32dcc8cd6c50e019ccf9dba61
bpopov <byurinov1@gmail.com>
2019-01-03 21:48:34 +0100

    [FIX] If given capacity was equal to zero it wasn't properly registered. Vector data is now null initialized.

--- srcs/ft_vector/ft_vector_init.c
+++ srcs/ft_vector/ft_vector_init.c
@@ -6,7 +6,7 @@
 /*   By: bopopovi <bopopovi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/20 02:50:01 by bopopovi          #+#    #+#             */
-/*   Updated: 2018/12/30 19:22:00 by bopopovi         ###   ########.fr       */
+/*   Updated: 2019/01/03 21:19:38 by bopopovi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,10 +20,10 @@ t_vect	*ft_vector_init(size_t data_size, size_t capacity)
 		ft_perror("ft_vector_init: vector allocation failed.");
 	if (capacity == 0)
 		capacity = VECTOR_INITIAL_SIZE;
-	else
-		new->capacity = capacity;
-	if (!(new->data = malloc(data_size * capacity)))
+	new->capacity = capacity;
+	if (!(new->data = malloc(data_size * new->capacity)))
 		ft_perror("ft_vector_init: vector data's allocation failed.");
+	ft_bzero(new->data, data_size * new->capacity);
 	new->data_size = data_size;
 	new->size = 0;
 	return (new);
