732db3ee6dea8fb43c8847bfea5aec3006519095
bpopov <byurinov1@gmail.com>
2019-01-03 21:42:01 +0100

    [MOD][FIX?] Cleaner method for cleaning collisions from table's entry

--- srcs/ft_hash/ft_hash_freetable.c
+++ srcs/ft_hash/ft_hash_freetable.c
@@ -6,7 +6,7 @@
 /*   By: bopopovi <bopopovi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/13 17:40:15 by bopopovi          #+#    #+#             */
-/*   Updated: 2018/12/30 16:46:35 by bopopovi         ###   ########.fr       */
+/*   Updated: 2019/01/03 21:19:51 by bopopovi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,11 +22,11 @@ void	ft_hash_freetable(t_htable **t, void (*d)(void *))
 	{
 		if ((*t)->table[i])
 		{
-			while ((*t)->table[i] != NULL)
+			while ((*t)->table[i]->next != NULL)
 			{
-				ptr = (*t)->table[i]->next;
-				ft_hashdel((*t)->table[i], d);
-				(*t)->table[i] = ptr;
+				ptr = (*t)->table[i]->next->next;
+				ft_hashdel((*t)->table[i]->next, d);
+				(*t)->table[i]->next = ptr;
 			}
 		}
 		i++;
