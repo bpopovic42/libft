e7f16b5340d4ace431dcdb9bb77168c6e4733def
Boris POPOVIC <bopopovi@student.42.fr>
2019-01-04 14:41:47 +0100

    [FIX] First link a of a collision chain wasn't freed

--- srcs/ft_hash/ft_hash_freetable.c
+++ srcs/ft_hash/ft_hash_freetable.c
@@ -6,7 +6,7 @@
 /*   By: bopopovi <bopopovi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/13 17:40:15 by bopopovi          #+#    #+#             */
-/*   Updated: 2019/01/03 21:19:51 by bopopovi         ###   ########.fr       */
+/*   Updated: 2019/01/04 14:38:00 by bopopovi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,6 +28,7 @@ void	ft_hash_freetable(t_htable **t, void (*d)(void *))
 				ft_hashdel((*t)->table[i]->next, d);
 				(*t)->table[i]->next = ptr;
 			}
+			ft_hashdel((*t)->table[i], d);
 		}
 		i++;
 	}
