cc764da6da393753d2825df157532b7ca1b45a4f
Boris POPOVIC <bopopovi@student.42.fr>
2019-02-21 18:30:31 +0100

    [MOD] Simplified function and added a comment.

--- srcs/ft_mem/ft_memcpy.c
+++ srcs/ft_mem/ft_memcpy.c
@@ -6,19 +6,24 @@
 /*   By: bopopovi <bopopovi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/04/14 14:23:22 by bopopovi          #+#    #+#             */
-/*   Updated: 2018/04/25 16:42:43 by bopopovi         ###   ########.fr       */
+/*   Updated: 2019/02/20 17:44:21 by bopopovi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+/*
+** Copy n bytes, byte per byte, from src into dst, don't forget additional
+** NULL byte if you copy strings !
+*/
+
 void	*ft_memcpy(void *dst, const void *src, size_t n)
 {
-	t_uchar *dst_ptr;
-	t_uchar *src_ptr;
+	char *dst_ptr;
+	char *src_ptr;
 
-	dst_ptr = (t_uchar*)dst;
-	src_ptr = (t_uchar*)src;
+	dst_ptr = (char*)dst;
+	src_ptr = (char*)src;
 	while (n--)
 		*dst_ptr++ = *src_ptr++;
 	return (dst);
