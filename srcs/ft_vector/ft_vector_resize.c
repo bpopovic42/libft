25ad47a98f6a851f67c5130d645c06c1f87a0af6
bpopov <byurinov1@gmail.com>
2019-03-01 16:41:37 +0100

    [FIX] Wrong variable name.

--- srcs/ft_vector/ft_vector_resize.c
+++ srcs/ft_vector/ft_vector_resize.c
@@ -6,7 +6,7 @@
 /*   By: bopopovi <bopopovi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/20 03:06:50 by bopopovi          #+#    #+#             */
-/*   Updated: 2019/03/01 16:11:02 by bopopovi         ###   ########.fr       */
+/*   Updated: 2019/03/01 16:41:20 by bopopovi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ int		ft_vector_resize(t_vect *v)
 	size_t	current_size;
 	void	*new_memory_block;
 
-	current_size = (v->data * v->size);
+	current_size = (v->data_size * v->size);
 	new_size = (v->data_size * (v->capacity * 2));
 	new_memory_block = ft_realloc(v->data, current_size, new_size);
 	if (!new_memory_block)
