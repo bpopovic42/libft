f78da7c4dba1c42a43a2b2d4c726bc6aef6c5572
Boris POPOVIC <bopopovi@student.42.fr>
2019-03-04 15:16:55 +0100

    [FIX] Function 'ft_realloc' wasn't properly replacing the initial memory pointer, resolved by using a double pointer, function now returns an error code instead of the newly allocated memory block.

--- srcs/ft_mem/ft_realloc.c
+++ srcs/ft_mem/ft_realloc.c
@@ -6,23 +6,23 @@
 /*   By: bopopovi <bopopovi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/20 03:13:38 by bopopovi          #+#    #+#             */
-/*   Updated: 2019/03/01 18:45:32 by bopopovi         ###   ########.fr       */
+/*   Updated: 2019/03/04 15:12:57 by bopopovi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	*ft_realloc(void *ptr, size_t size, size_t new_size)
+int		ft_realloc(void **ptr, size_t size, size_t new_size)
 {
 	void *new_ptr;
 
 	if (!(new_ptr = ft_memalloc(new_size)))
-		return (NULL);
-	if (ptr)
+		return (-1);
+	if (*ptr)
 	{
-		ft_memcpy(new_ptr, ptr, size);
-		ft_memdel(&ptr);
+		ft_memcpy(new_ptr, *ptr, size);
+		ft_memdel(ptr);
 	}
-	ptr = new_ptr;
-	return (ptr);
+	*ptr = new_ptr;
+	return (0);
 }
