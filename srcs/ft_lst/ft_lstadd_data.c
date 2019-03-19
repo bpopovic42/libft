517f8ac1200be1e312ca4445ee3169263d51a75b
Boris POPOVIC <bopopovi@student.42.fr>
2019-03-19 19:44:22 +0100

    [RM] Debugging code.

--- srcs/ft_lst/ft_lstadd_data.c
+++ srcs/ft_lst/ft_lstadd_data.c
@@ -6,12 +6,11 @@
 /*   By: bopopovi <bopopovi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/28 18:11:06 by bopopovi          #+#    #+#             */
-/*   Updated: 2019/03/13 21:10:08 by bopopovi         ###   ########.fr       */
+/*   Updated: 2019/03/19 19:44:03 by bopopovi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-#include "ft_printf.h"
 
 int		ft_lstadd_data(t_list *alst, void *data, size_t data_size)
 {
@@ -20,7 +19,6 @@ int		ft_lstadd_data(t_list *alst, void *data, size_t data_size)
 	new = NULL;
 	if (!(new = ft_node_new(data, data_size)))
 		return (-1);
-	ft_printf("adding node %p\n", new);
 	ft_lstadd(alst, new);
 	return (0);
 }
