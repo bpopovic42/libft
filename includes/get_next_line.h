f5f71a141b4e2926c019f7f3c2b8ed791ba3f3c5
Boris POPOVIC <bopopovi@student.42.fr>
2019-03-25 19:56:15 +0100

    [MOD] Imported re-written 'get_next_line' function.

--- includes/get_next_line.h
+++ includes/get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: bopopovi <bopopovi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/05/18 20:45:59 by bopopovi          #+#    #+#             */
-/*   Updated: 2018/06/01 19:26:21 by bopopovi         ###   ########.fr       */
+/*   Updated: 2019/03/25 00:13:37 by bopopovi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,23 @@
 # define GET_NEXT_LINE_H
 
 # define BUFF_SIZE 1024
+# define GNL_ERROR -1
+# define GNL_EOF 0
+# define GNL_READ 1
 
 # include "libft.h"
 
-typedef struct		s_fd
+
+typedef struct	s_gnl
 {
-	int				fd;
-	char			*buff;
-}					t_fd;
+	char		*data;
+	size_t		data_len;
+	char		buff[BUFF_SIZE + 1];
+	int			newline_index;
+	size_t		eol_index;
+	int			status;
+	int			has_newline;
+}				t_gnl;
 
 int					get_next_line(const int fd, char **line);
 
--- srcs/ft_io/get_next_line.c
+++ srcs/ft_io/get_next_line.c
@@ -6,44 +6,99 @@
 /*   By: bopopovi <bopopovi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/05/18 20:42:42 by bopopovi          #+#    #+#             */
-/*   Updated: 2019/03/22 16:49:34 by bopopovi         ###   ########.fr       */
+/*   Updated: 2019/03/24 23:10:06 by bopopovi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 #include <unistd.h>
 
+static void	init_reader(t_gnl *reader)
+{
+	reader->data = NULL;
+	reader->data_len = 0;
+	reader->eol_index = 0;
+	reader->has_newline = 0;
+	reader->newline_index = 0;
+	reader->status = 0;
+}
+
+static int	add_file_remains_to_dict(const int fd, t_gnl *reader, t_dict **list)
+{
+	char	*file_remains;
+	size_t	remains_len;
+
+	if (reader->has_newline)
+	{
+		file_remains = reader->data + (reader->newline_index + 1);
+		remains_len = ft_strlen(file_remains) + 1;
+		if (ft_dictadd(list, fd, file_remains, remains_len) < 0)
+			return (-1);
+	}
+	return (0);
+}
+
+static int	reader_has_eol(t_gnl *reader)
+{
+	if (reader->status == 0)
+	{
+		if (reader->data)
+		{
+			reader->data_len = ft_strlen(reader->data);
+			reader->eol_index = reader->data_len;
+		}
+		return (1);
+	}
+	else if ((reader->newline_index = ft_strchri(reader->data, '\n')) >= 0)
+	{
+		reader->has_newline = 1;
+		reader->eol_index = reader->newline_index;
+		reader->data_len = ft_strlen(reader->data);
+		return (1);
+	}
+	return (0);
+}
+
+static void	read_file_until_eol(const int fd, t_gnl *reader)
+{
+	reader->status = GNL_READ;
+	while (!reader_has_eol(reader))
+	{
+		if ((reader->status = (int)read(fd, reader->buff, BUFF_SIZE)) > 0)
+		{
+			reader->buff[reader->status] = '\0';
+			if (!(reader->data = ft_strappend(reader->data, reader->buff)))
+				reader->status = GNL_ERROR;
+		}
+		if (reader->status < 0)
+			break ;;
+	}
+}
+
 int			get_next_line(const int fd, char **line)
 {
 	static t_dict	*list;
-	char			*rest;
-	char			buff[BUFF_SIZE + 1];
-	int				i;
-	int				ret;
+	t_gnl			reader;
 
-	ret = 1;
-	if (fd < 0 || !(line))
-		return (-1);
-	rest = (char*)ft_dictget(&list, fd);
-	while ((i = ft_strchri(rest, '\n')) < 0 && ret)
+	init_reader(&reader);
+	if (fd >= 0 && line)
 	{
-		if ((ret = (int)read(fd, buff, BUFF_SIZE)) < 0)
-			return (-1);
-		buff[ret] = '\0';
-		if (!(rest = ft_strappend(rest, buff)))
-			return (-1);
+		reader.data = (char*)ft_dictget(&list, fd);
+		read_file_until_eol(fd, &reader);
+		if (reader.status >= 0 && reader.data_len > 0)
+		{
+			if (!(*line = ft_strsub(reader.data, 0, reader.eol_index)))
+				reader.status = GNL_ERROR;
+			else if (add_file_remains_to_dict(fd, &reader, &list) < 0)
+				reader.status = GNL_ERROR;
+			else
+				reader.status = GNL_READ;
+			ft_strdel(&reader.data);
+		}
+		else if (reader.status == 0 && reader.data_len == 0)
+			reader.status = GNL_EOF;
 	}
-	if (rest[0])
-	{
-		if (!(*line = ft_strsub(rest, 0, i >= 0 ? (size_t)i : ft_strlen(rest))))
-			return (-1);
-	}
-	if (i++ >= 0)
-	{
-		if (ft_dictadd(&list, fd, rest + i, ft_strlen(rest + i) + 1) < 0)
-			return (-1);
-	}
-	ret = (rest[0]) ? 1 : ret;
-	ft_strdel(&rest);
-	return (ret);
+	else
+		reader.status = GNL_ERROR;
+	return (reader.status);
 }
