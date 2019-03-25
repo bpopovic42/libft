2a4278fa94c06fbf4cfcbd2a8d41cacb53e1b20d
bpopov <byurinov1@gmail.com>
2019-03-25 17:10:38 +0100

    [MOD] Optimized 'ft_memcpy' function.

--- srcs/ft_mem/ft_memcpy.c
+++ srcs/ft_mem/ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: bopopovi <bopopovi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/04/14 14:23:22 by bopopovi          #+#    #+#             */
-/*   Updated: 2019/02/20 17:44:21 by bopopovi         ###   ########.fr       */
+/*   Updated: 2019/03/25 00:22:18 by bopopovi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,16 +15,30 @@
 /*
 ** Copy n bytes, byte per byte, from src into dst, don't forget additional
 ** NULL byte if you copy strings !
+** t_word must be a power of 2 for AND modulo to work
 */
+
+typedef int t_word;
 
 void	*ft_memcpy(void *dst, const void *src, size_t n)
 {
-	char *dst_ptr;
-	char *src_ptr;
+	size_t		len;
+	size_t		wsize;
+	char		*dst_ptr;
+	const char	*src_ptr;
 
 	dst_ptr = (char*)dst;
 	src_ptr = (char*)src;
-	while (n--)
+	wsize = sizeof(t_word);
+	len = n / wsize;
+	while (len--)
+	{
+		*(t_word*)dst_ptr = *(t_word*)src_ptr;
+		dst_ptr += wsize;
+		src_ptr += wsize;
+	}
+	len = n & (wsize - 1);
+	while (len--)
 		*dst_ptr++ = *src_ptr++;
 	return (dst);
 }
