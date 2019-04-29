a807b8f8c6086b9b895411c9c6366ae31990ac94
Boris POPOVIC <bopopovi@student.42.fr>
2019-04-29 18:47:00 +0200

    [MOD] Optimized ft_strncmp.

--- srcs/ft_str/ft_strncmp.c
+++ srcs/ft_str/ft_strncmp.c
@@ -6,28 +6,48 @@
 /*   By: bopopovi <bopopovi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/04/06 17:49:43 by bopopovi          #+#    #+#             */
-/*   Updated: 2018/04/25 16:23:22 by bopopovi         ###   ########.fr       */
+/*   Updated: 2019/04/29 17:06:08 by bopopovi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+int		compare_blocks(const char *p1, const char *p2, size_t block, size_t n)
+{
+	size_t i;
+
+	i = block * sizeof(t_word);
+	while (i < n)
+	{
+		if (p1[i] ^ p2[i] || p1[i] == 0 || p2[i] == 0)
+			break;
+		i++;
+	}
+	return (int)((unsigned char)p1[i] - (unsigned char)p2[i]);
+}
+
 int		ft_strncmp(const char *s1, const char *s2, size_t n)
 {
-	const t_uchar *p1;
-	const t_uchar *p2;
+	t_word	*p1;
+	t_word	*p2;
+	size_t	block;
+	size_t	len;
 
-	p1 = (const t_uchar*)s1;
-	p2 = (const t_uchar*)s2;
+	p1 = (t_word*)s1;
+	p2 = (t_word*)s2;
+	block = 0;
 	if (n != 0)
 	{
-		while (n--)
+		len = n / sizeof(t_word);
+		if (n <= sizeof(t_word))
+			len = 0;
+		while (block < len)
 		{
-			if (*p1 != *p2++)
-				return (*p1 - *--p2);
-			if (*p1++ == 0)
-				return (0);
+			if (p1[block] ^ p2[block])
+				return (compare_blocks(s1, s2, block, n));
+			block++;
 		}
+		return (compare_blocks(s1, s2, block, n));
 	}
 	return (0);
 }
