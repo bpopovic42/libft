d30e18803001dfabb4bbee55da79a2481dab6909
Boris POPOVIC <bopopovi@student.42.fr>
2019-05-13 19:46:14 +0200

    [FIX] Color weren't parsed properly because wrong length was passed to 'ft_strncmp'. (classical index '0' mistake)

--- srcs/ft_printf/ft_printf_color.c
+++ srcs/ft_printf/ft_printf_color.c
@@ -6,7 +6,7 @@
 /*   By: bopopovi <bopopovi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/08/30 17:04:17 by bopopovi          #+#    #+#             */
-/*   Updated: 2018/09/06 16:48:24 by bopopovi         ###   ########.fr       */
+/*   Updated: 2019/05/13 19:43:46 by bopopovi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,19 +21,19 @@
 int		ft_printf_color(t_ptf *ptf)
 {
 	ft_printf_dump_fmt(ptf);
-	if (!ft_strncmp(ptf->fmt.format, "{eoc}", 5))
+	if (!ft_strncmp(ptf->fmt.format, "{eoc}", 4))
 		ft_printf_buff_cat(ptf, FT_PRINTF_EOC, 5);
-	else if (!ft_strncmp(ptf->fmt.format, "{red}", 5))
+	else if (!ft_strncmp(ptf->fmt.format, "{red}", 4))
 		ft_printf_buff_cat(ptf, FT_PRINTF_RED, 5);
-	else if (!ft_strncmp(ptf->fmt.format, "{green}", 7))
+	else if (!ft_strncmp(ptf->fmt.format, "{green}", 6))
 		ft_printf_buff_cat(ptf, FT_PRINTF_GRN, 5);
-	else if (!ft_strncmp(ptf->fmt.format, "{blue}", 6))
+	else if (!ft_strncmp(ptf->fmt.format, "{blue}", 5))
 		ft_printf_buff_cat(ptf, FT_PRINTF_BLU, 5);
-	else if (!ft_strncmp(ptf->fmt.format, "{yellow}", 8))
+	else if (!ft_strncmp(ptf->fmt.format, "{yellow}", 7))
 		ft_printf_buff_cat(ptf, FT_PRINTF_YLW, 5);
-	else if (!ft_strncmp(ptf->fmt.format, "{cyan}", 6))
+	else if (!ft_strncmp(ptf->fmt.format, "{cyan}", 5))
 		ft_printf_buff_cat(ptf, FT_PRINTF_CYA, 5);
-	else if (!ft_strncmp(ptf->fmt.format, "{magenta}", 9))
+	else if (!ft_strncmp(ptf->fmt.format, "{magenta}", 8))
 		ft_printf_buff_cat(ptf, FT_PRINTF_MAG, 5);
 	else
 		return (1);
