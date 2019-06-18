94035250957aa7abc79551929245c6170fbae858
Boris POPOVIC <bopopovi@student.42.fr>
2019-06-18 21:46:43 +0200

    [NORM]

--- srcs/ft_hash/ft_hashpush.c
+++ srcs/ft_hash/ft_hashpush.c
@@ -6,7 +6,7 @@
 /*   By: bopopovi <bopopovi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/05/25 00:04:55 by bopopovi          #+#    #+#             */
-/*   Updated: 2018/12/03 19:54:30 by bopopovi         ###   ########.fr       */
+/*   Updated: 2019/06/18 21:46:39 by bopopovi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 void	ft_hashpush(t_htable *table, t_hash *elem)
 {
 	int		hash;
-	t_hash *ptr;
+	t_hash	*ptr;
 
 	hash = ft_hash(table, elem->key);
 	if (table->table[hash] == NULL)
